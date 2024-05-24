03a81880d48249017417b011e46ce8e18bb6afd5
Luis Alberto Donado <luisinodnd@gmail.com>
2024-05-24 11:15:12 -0500

    Migraciones

---
+++ database/migrations/2024_05_24_154813_create_pacientes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pacientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('especie');
+            $table->string('raza');
+            $table->integer('edad');
+            $table->float('peso');
+            $table->unsignedBigInteger('historial_medico_id');
+            $table->timestamps();
+
+            $table->foreign('historial_medico_id')->references('id')->on('historiales_medicos')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pacientes');
+    }
+};
